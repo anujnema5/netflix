c2b4e22f9addee3f7fa53c36caef1062a68a5b1c
Anujnema <anujnema63@gmail.com>
2023-09-27 14:19:59 +0530

    Some error needs to be fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 import React from 'react'
+import { auth } from '../utils/firebase';
 
 function Header() {
+    const navigate = useNavigate();
+    const user = useSelector(state => state.user)
+
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            navigate("/")
+        }).catch((error) => {
+        });
+    }
+
     return (
-        <div className="absolute px-8 py-1 bg-gradient-to-b ">
+        <div className="absolute w-screen px-8 py-1 bg-gradient-to-b from-black z-10 flex justify-between">
             <img className='w-48' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+
+            {user && <div className="flex items-center">
+                <img className='w-12 h-12' src={user?.photoURL} alt="usericon" />
+                <button className='bg-red-500 px-2 py-1 font-bold rounded-md text-white' onClick={handleSignOut}>Sign out</button>
+            </div>}
         </div>
     )
 }
